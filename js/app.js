362719aa831f8838f781b93465267abc3bf036af
Teisia Park <teisia@gmail.com>
2016-02-24 14:24:52 -0700

    finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 var app = angular.module('omdbApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'mainController'
+    }),
+    $routeProvider
+      .when('/:movieID', {
+        templateUrl: 'partials/movie.html',
+        controller: 'showController'
+      })
+});
---
+++ js/controllers.js
@@ -0,0 +1,17 @@
+app.controller("mainController", function($scope, $routeParams, $http) {
+  $scope.getMovie = function() {
+    $http.get("http://www.omdbapi.com/?s=" + $scope.movieTitle).then(function(data){
+      $scope.movies = [];
+      data.data.Search.forEach(function(movie) {
+        $scope.movies.push(movie)
+      })
+    })
+  }
+});
+
+app.controller("showController", function($scope, $routeParams, $http) {
+    $http.get("http://www.omdbapi.com/?i=" + $routeParams.movieID).then(function(data){
+      $scope.showMovies = [];
+      $scope.showMovies.push(data.data)
+  })
+})
